909de9c8b939b41bc0aa775869fac599e6318132
aleng-lol <yilingding01@gmail.com>
2025-07-08 18:56:35 -0400

    Updated Timekeeper.cs

--- Assets/Scripts/Timekeeper.cs
+++ Assets/Scripts/Timekeeper.cs
@@ -9,19 +9,56 @@ public class Timekeeper : MonoBehaviour
 {
     float time;
     TimeSpan timeSpan;
+    string timeString;
+    bool isRunning = false;
     [SerializeField] TMP_Text timerText;
     // Start is called before the first frame update
     void Start()
     {
         time = 0;
-
+        StartTime();
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
-        timeSpan = TimeSpan.FromSeconds(time);
-        timerText.text = string.Format("Time On Stage: {0:D2}:{1:D2}", timeSpan.Minutes, timeSpan.Seconds);
+        //checks if the time is supposed to be running
+        if (isRunning)
+        {
+            //increase time
+            time += Time.deltaTime;
+
+            //sets up TimeSpan object (for formatting)
+            timeSpan = TimeSpan.FromSeconds(time);
+
+            //formats the time nicely
+            timeString = string.Format("Time On Stage: {0:D2}:{1:D2}", timeSpan.Minutes, timeSpan.Seconds);
+
+            //sets the text to the time
+            timerText.text = timeString;
+        }
+        //testing stopping the time
+        // if (Input.GetKeyDown(KeyCode.A))
+        // {
+        //     StopTime();
+        // }
+    }
+
+    public void StopTime()
+    {
+        isRunning = false;
+    }
+
+
+    public void StartTime()
+    {
+        isRunning = true;
+    }
+
+
+    //returns the time as a string
+    public string getTime()
+    {
+        return timeString;
     }
 }
