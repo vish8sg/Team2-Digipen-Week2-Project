65273a219e96f1e2292fb9c910508195457c334e
vish8sg <tvishnu2008@gmail.com>
2025-07-08 14:22:45 -0400

    Circular Motion Controller V0
    added a basic circular motion script that can be attached to an object to move it clockwise, counterclockwise, and accelerate tangentially.

---
+++ Assets/Scripts/CircularMotion.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CircularMotion : MonoBehaviour
+{
+
+    [SerializeField] Transform center = null;
+    [SerializeField] float speed = 10f;
+    [SerializeField] float angularAcceleration = 10f;
+    [SerializeField] float maxAngularSpeed = 50f;
+
+    float radius = 0f;
+
+    float angularSpeed = 0f;
+    float angle = 0f;
+
+    private void Start()
+    {
+        if (center == null) { return; }
+        radius = Vector3.Distance(center.position, transform.position);
+    }
+
+    private void Update()
+    {
+        HandleInput();
+
+        angle += angularSpeed * Time.deltaTime;
+
+        //Calculate new positon on circle
+        Vector2 offset = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * radius;
+        transform.position = (Vector2) center.position + offset;
+
+        //Rotate to face tangent direction - (rotations are complicated so used chatgpt)
+        Vector2 tangent = new Vector2(-Mathf.Sin(angle), Mathf.Cos(angle)) * Mathf.Sign(angularSpeed);
+        float angleDeg = Mathf.Atan2(tangent.y, tangent.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angleDeg);
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            angularSpeed -= angularAcceleration * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            angularSpeed += angularAcceleration * Time.deltaTime;
+        } 
+
+        angularSpeed = Mathf.Clamp(angularSpeed, -maxAngularSpeed, maxAngularSpeed);
+    }
+
+}
