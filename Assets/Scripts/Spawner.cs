560f0d6dbcb18155f51e463e9ebc93345a8fd143
vish8sg <tvishnu2008@gmail.com>
2025-07-09 13:39:23 -0400

    Added working spawner script
    Spawner can be attached to empty game object to spawn random enemies in the list every few seconds at random points along the edges of the screen

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] float spawnRate = 1f;
+    [SerializeField] private GameObject[] enemyPrefabs = null;
+    [SerializeField] float launchForce = 10f;
+
+    private bool canSpawn = true;
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        WaitForSeconds wait = new WaitForSeconds(spawnRate);
+
+        while (true)
+        {
+            yield return wait;
+
+            int randomEnemyIndex = Random.Range(0, 1);
+            GameObject projectile = Instantiate(enemyPrefabs[randomEnemyIndex], GetRandomScreenEdgePosition(), Quaternion.identity);
+
+            Vector2 dirToOrigin = (Vector2)(-projectile.transform.position.normalized);
+            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+            rb.AddForce(dirToOrigin * launchForce, ForceMode2D.Impulse);
+        }
+    }
+
+    Vector3 GetRandomScreenEdgePosition()
+    {
+        Camera cam = Camera.main;
+
+        // Step 1: Pick a side randomly (0=left, 1=right, 2=top, 3=bottom)
+        int side = UnityEngine.Random.Range(0, 2);
+
+        // Step 2: Get screen dimensions
+        float screenX = 0f;
+        float screenY = 0f;
+
+        switch (side)
+        {
+            case 0: // Left
+                screenX = 0f;
+                screenY = UnityEngine.Random.Range(0f, Screen.height);
+                break;
+            case 1: // Right
+                screenX = Screen.width;
+                screenY = UnityEngine.Random.Range(0f, Screen.height);
+                break;
+            case 2: // Top
+                screenX = UnityEngine.Random.Range(0f, Screen.width);
+                screenY = Screen.height;
+                break;
+            case 3: // Bottom
+                screenX = UnityEngine.Random.Range(0f, Screen.width);
+                screenY = 0f;
+                break;
+        }
+
+        // Step 3: Convert screen point to world point
+        Vector3 screenPoint = new Vector3(screenX, screenY, cam.nearClipPlane);
+        Vector3 worldPoint = cam.ScreenToWorldPoint(screenPoint);
+
+        // Optional: Zero out z for 2D
+        worldPoint.z = 0f;
+
+        return worldPoint;
+    }
+
+}
