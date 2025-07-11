4321f7668bde2f18bd19030c93487e0f45d15622
vish8sg <tvishnu2008@gmail.com>
2025-07-11 13:52:39 -0400

    Update Paddle.cs

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -7,7 +7,7 @@ public class Paddle : MonoBehaviour
     [Tooltip("Hit stop in seconds when paddle collides with projectile")]
     [SerializeField] float hitStop = 0.05f;
 
-    [Tooltip("How much red gets added to the paddle when it collides with tomato")]
+    [Tooltip("How much red gets added to the paddle when it collides with tomato - Range is from 0-1")]
     [SerializeField] float tomatoSplotchAmount = 0.1f;
 
     private void OnTriggerEnter2D(Collider2D other)
