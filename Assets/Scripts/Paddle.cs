edd6aaf42715efa9b131189b298e26e0c61ace41
vish8sg <tvishnu2008@gmail.com>
2025-07-11 13:50:00 -0400

    Update Paddle.cs

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -7,11 +7,15 @@ public class Paddle : MonoBehaviour
     [Tooltip("Hit stop in seconds when paddle collides with projectile")]
     [SerializeField] float hitStop = 0.05f;
 
+    [Tooltip("How much red gets added to the paddle when it collides with tomato")]
+    [SerializeField] float tomatoSplotchAmount = 0.1f;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "BadProjectile")
         {
             TimeFreezer.FreezeTime(hitStop);
+            GetComponent<SpriteRenderer>().color = Color.Lerp(GetComponent<SpriteRenderer>().color, Color.red, tomatoSplotchAmount);
         }
     }
 }
